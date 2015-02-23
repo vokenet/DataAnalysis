8ae63a7cf1e3e20dd6252b2eec36f64147be397e
malie1982 <mal_wolf_2008@126.com>
2015-02-23 22:40:10 +0800

    # -- Create system install folder # --

--- includes/DA.sql
+++ includes/DA.sql
@@ -24,10 +24,11 @@ DROP TABLE IF EXISTS `commodities`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `commodities` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `purchase_date` date NOT NULL,
-  `purchase_cost` double(12,2) DEFAULT '0.00',
+  `purchase_date` date NOT NULL COMMENT '购买日期',
+  `commodities_name` varchar(128) DEFAULT NULL COMMENT '耗材名称',
+  `purchase_cost` double(12,2) DEFAULT '0.00' COMMENT '购买费用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='耗材明细表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,17 +49,17 @@ DROP TABLE IF EXISTS `contact`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contact` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contact_name` varchar(60) DEFAULT NULL,
-  `contact_tel` varchar(30) DEFAULT NULL,
-  `contact_Position` varchar(60) DEFAULT NULL,
-  `e_mail` varchar(30) DEFAULT NULL,
-  `qq` varchar(30) DEFAULT NULL,
-  `other_tel` varchar(60) DEFAULT NULL,
-  `company_name` varchar(128) DEFAULT NULL,
-  `company_addr` varchar(256) DEFAULT NULL,
-  `company_Nature` varchar(128) DEFAULT NULL,
+  `contact_name` varchar(60) DEFAULT NULL COMMENT '姓名',
+  `contact_tel` varchar(30) DEFAULT NULL COMMENT '手机号',
+  `contact_Position` varchar(60) DEFAULT NULL COMMENT '职务',
+  `e_mail` varchar(30) DEFAULT NULL COMMENT '电子邮箱',
+  `qq` varchar(30) DEFAULT NULL COMMENT 'qq号',
+  `other_tel` varchar(60) DEFAULT NULL COMMENT '其他联系方式',
+  `company_name` varchar(128) DEFAULT NULL COMMENT '公司名称',
+  `company_addr` varchar(256) DEFAULT NULL COMMENT '公司地址',
+  `company_Nature` varchar(128) DEFAULT NULL COMMENT '公司性质',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='联系人';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,24 +80,24 @@ DROP TABLE IF EXISTS `electronic_sign_in`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `electronic_sign_in` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sign_date` date NOT NULL,
-  `sign_mach_type` varchar(5) DEFAULT NULL,
-  `sign_mach_count` int(11) DEFAULT '0',
-  `sign_cost_income` double(12,2) DEFAULT '0.00',
-  `lottery_cost_income` double(12,2) DEFAULT '0.00',
-  `photo_print_cost_income` double(12,2) DEFAULT '0.00',
-  `fare_cost_income` double(12,2) DEFAULT '0.00',
-  `Invoice_cost_income` double(12,2) DEFAULT '0.00',
-  `fare_cost_outcome` double(12,2) DEFAULT '0.00',
-  `meals_outcome` double(12,2) DEFAULT '0.00',
-  `Invoice_cost_outcome` double(12,2) DEFAULT '0.00',
-  `other_cost_outcome` double(12,2) DEFAULT '0.00',
-  `other_cost_comments` varchar(128) DEFAULT NULL,
-  `Surplus` double(12,2) DEFAULT '0.00',
-  `Contact_id` varchar(10) DEFAULT NULL,
-  `use_unit` varchar(128) DEFAULT NULL,
+  `sign_date` date NOT NULL COMMENT '签到日期',
+  `sign_mach_type` varchar(5) DEFAULT NULL COMMENT '签到机类型',
+  `sign_mach_count` int(11) DEFAULT '0' COMMENT '签到机数量',
+  `sign_cost_income` double(12,2) DEFAULT '0.00' COMMENT '签到收入',
+  `lottery_cost_income` double(12,2) DEFAULT '0.00' COMMENT '抽奖收入',
+  `photo_print_cost_income` double(12,2) DEFAULT '0.00' COMMENT '照片打印收入',
+  `fare_cost_income` double(12,2) DEFAULT '0.00' COMMENT '运费收入',
+  `Invoice_cost_income` double(12,2) DEFAULT '0.00' COMMENT '发票收入',
+  `fare_cost_outcome` double(12,2) DEFAULT '0.00' COMMENT '运费支出',
+  `meals_outcome` double(12,2) DEFAULT '0.00' COMMENT '餐费支出',
+  `Invoice_cost_outcome` double(12,2) DEFAULT '0.00' COMMENT '发票支出',
+  `other_cost_outcome` double(12,2) DEFAULT '0.00' COMMENT '其他费用支出',
+  `other_cost_comments` varchar(128) DEFAULT NULL COMMENT '其他费用支出说明',
+  `Surplus` double(12,2) DEFAULT '0.00' COMMENT '盈余',
+  `Contact_id` varchar(10) DEFAULT NULL COMMENT '联系人编号',
+  `use_unit` varchar(128) DEFAULT NULL COMMENT '使用单位',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='电子签到明细表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,13 +118,13 @@ DROP TABLE IF EXISTS `p_code`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `p_code` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `CODE_TYPE` varchar(60) DEFAULT NULL,
-  `CODE_DESC` varchar(60) DEFAULT NULL,
-  `CODE` varchar(5) DEFAULT NULL,
-  `ORD` int(11) DEFAULT NULL,
-  `ENABLE_FLAG` varchar(5) DEFAULT NULL,
+  `CODE_TYPE` varchar(60) DEFAULT NULL COMMENT '类型',
+  `CODE_DESC` varchar(60) DEFAULT NULL COMMENT '备注',
+  `CODE` varchar(5) DEFAULT NULL COMMENT '编码',
+  `ORD` int(11) DEFAULT NULL COMMENT '顺序',
+  `ENABLE_FLAG` varchar(5) DEFAULT NULL COMMENT '启用标志',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='代码表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,4 +145,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-02-23 22:18:49
+-- Dump completed on 2015-02-23 22:39:26
